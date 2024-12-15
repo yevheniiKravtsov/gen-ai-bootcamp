ea8be13a255200ee4140f9578e71872a94fa5094
yevheniiKravtsov <y.k.kravtsov@gmail.com>
2024-12-15 16:48:27 +0200

    task2 chat history

--- tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/service/GenAiService.java
+++ tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/service/GenAiService.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 public interface GenAiService {
     GenAiResponse getGeneratedResponse(String prompt);
+
+    GenAiResponse processWithHistory(String prompt);
 }
